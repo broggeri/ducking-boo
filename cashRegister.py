d1c628531edeea7f4a59391246a0fa80b369a18b
Remi Caput <r.caput@criteo.com>
2014-11-04 15:34:05 +0100

    Switch to spaces.

--- cashRegister.py
+++ cashRegister.py
@@ -13,17 +13,17 @@
 import json
 
 def input_fruit():
-	return [fruit.strip() for fruit in raw_input().split(',')]
+    return [fruit.strip() for fruit in raw_input().split(',')]
 
 def main():
     db = json.load(open('database.json'))
 
     total = 0
     while True:
-		for fruit in input_fruit():
-			if fruit in db:
-				total += db[fruit]*100
-		print total
+        for fruit in input_fruit():
+            if fruit in db:
+                total += db[fruit]*100
+        print total
 
 if __name__ == '__main__':
     main()
