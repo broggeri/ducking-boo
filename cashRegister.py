f86e7fac33d626c7f6aeeae957f998ddce46291e
Remi Caput <r.caput@criteo.com>
2014-11-04 15:03:13 +0100

    Add initial cash register coe.

---
+++ cashRegister.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      r.bauvin
+#
+# Created:     04/11/2014
+# Copyright:   (c) r.bauvin 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import json
+
+def main():
+    db = json.load(open('database.json'))
+
+
+    total = 0
+    while True:
+        fruit = input()
+        if fruit in db:
+            total += db[fruit]*100
+        print total
+
+if __name__ == '__main__':
+    main()
